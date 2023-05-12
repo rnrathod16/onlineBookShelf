c336e9eb2a56798b32239b5ebb2a963a2aee2e24
rnrathod16 <rnrathod16@gmail.com>
2023-05-12 13:04:01 +0530

    Added Service layer Functions

--- src/main/java/com/example/demo/service/Service_Declarations.java
+++ src/main/java/com/example/demo/service/Service_Declarations.java
@@ -1,5 +1,29 @@
 package com.example.demo.service;
 
-public interface Service_Declarations {
+import java.util.List;
 
+import com.example.demo.entity.Books;
+import com.example.demo.entity.Users;
+
+public interface Service_Declarations {
+	
+	public boolean login(String uemail, String upassword);
+	public void register(Users user);
+	
+	//Admin
+	
+	public void addBook(Books book);
+	public void updateBook(Books book);
+	
+	public void deleteBookById(int bid);
+	public void addStockToBooks(int bid);
+	
+	public List<Books> searchByCategory(int cid);
+	public void addBookByCategory(Books book, int cid);
+	
+	//Users
+	
+//	public List<Books> searchBook(String general);
+	
+	
 }
--- src/main/java/com/example/demo/service/Service_Implementation.java
+++ src/main/java/com/example/demo/service/Service_Implementation.java
@@ -1,5 +1,59 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.entity.Books;
+import com.example.demo.entity.Users;
+
 public class Service_Implementation implements Service_Declarations{
 
+	@Override
+	public boolean login(String uemail, String upassword) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void register(Users user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addBook(Books book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBook(Books book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBookById(int bid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addStockToBooks(int bid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Books> searchByCategory(int cid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addBookByCategory(Books book, int cid) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
